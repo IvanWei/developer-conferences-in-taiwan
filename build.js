9295e32240128e711795b1d5233f191a7e9de2ce
Ivan Wei <scorpionseed@gmail.com>
2020-10-16 17:31:54 +0800

    feat: 🎸 update seo

--- build.js
+++ build.js
@@ -9,6 +9,18 @@ const organizationData = require("./data/list-of-organizations.json").data;
 
 const data = Array.prototype.concat.call(conferenceData, [{"hr": ""}], organizationData);
 
+showdown.extension('targetlink', function() {
+  return [{
+    type: 'lang',
+    regex: /\[((?:\[[^\]]*]|[^\[\]])*)]\([ \t]*<?(.*?(?:\(.*?\).*?)?)>?[ \t]*((['"])(.*?)\4[ \t]*)?\)/g,
+    replace: function(wholematch, linkText, url) {
+      const result = `<a href="${url}" target="_blank" alt="${linkText}">${linkText}</a>`;
+
+      return result;
+    }
+  }];
+});
+
 switch (type) {
   case 'readme':
     fs.writeFile('README.md', json2md(data), (err) => {
@@ -18,12 +30,28 @@ switch (type) {
 
     break;
   case 'ghPage':
-    const converter = new showdown.Converter({tables: true});
+    const converter = new showdown.Converter({tables: true, extensions: ['targetlink']});
     const html = (`
-      <!DOCTYPE HTML><html>
+      <!DOCTYPE HTML>
+      <html lang="zh-hant">
       <head>
         <meta charset="utf-8">
         <title>Developer Conferences in Taiwan</title>
+        <meta name="description" content="統整開發相關研討會資訊" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content="Developer Conferences in Taiwan" />
+        <meta property="og:description" content="統整開發相關研討會資訊" />
+        <meta property="og:url" content="https://dcit.ivanwei.co" />
+        <meta property="og:site_name" content="Developer Conferences in Taiwan" />
+        <meta property="og:image" content="https://blog.ivanwei.co/images/2018/05/16/DCIT.png" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Developer Conferences in Taiwan" />
+        <meta name="twitter:description" content="統整開發相關研討會資訊" />
+        <meta name="twitter:image" content="https://blog.ivanwei.co/images/2018/05/16/DCIT.png" />
+
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/picnic">
       </head>
       <body>
