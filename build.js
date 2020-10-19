17efa94327248c909058a45e706b01e8e1628316
Ivan Wei <scorpionseed@gmail.com>
2020-10-19 12:42:53 +0800

    feat: 🎸 update html pages

--- build.js
+++ build.js
@@ -31,18 +31,19 @@ switch (type) {
     break;
   case 'ghPage':
     const converter = new showdown.Converter({tables: true, extensions: ['targetlink']});
-    const html = (`
+
+    const conferenceHtml = (`
       <!DOCTYPE HTML>
       <html lang="zh-tw">
       <head>
         <meta charset="utf-8">
         <title>Developer Conferences in Taiwan</title>
-        <meta name="description" content="統整開發相關研討會資訊" />
+        <meta name="description" content="統整研討會資訊" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Developer Conferences in Taiwan" />
-        <meta property="og:description" content="統整開發相關研討會資訊" />
+        <meta property="og:description" content="統整研討會資訊" />
         <meta property="og:url" content="https://dcit.ivanwei.co" />
         <meta property="og:site_name" content="Developer Conferences in Taiwan" />
         <meta property="og:image" content="https://blog.ivanwei.co/images/2018/05/16/DCIT.png" />
@@ -50,18 +51,57 @@ switch (type) {
 
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:title" content="Developer Conferences in Taiwan" />
-        <meta name="twitter:description" content="統整開發相關研討會資訊" />
+        <meta name="twitter:description" content="統整研討會資訊" />
         <meta name="twitter:image" content="https://blog.ivanwei.co/images/2018/05/16/DCIT.png" />
 
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/picnic">
       </head>
       <body>
-        ${converter.makeHtml(json2md(data))}
+        <a class="button" href="/organization.html">查看活動籌備單位</a>
+        <a class="button" href="https://github.com/IvanWei/developer-conferences-in-taiwan/blob/master/data/list-of-organizations.json">新增活動籌備單位</a>
+        ${converter.makeHtml(json2md(conferenceData))}
       </body>
       </html>
     `).replace(/(\r\n|\n|\r|\ \ )/gm, '');
 
-    fs.writeFile('docs/index.html', html, (err) => {
+    fs.writeFile('docs/index.html', conferenceHtml, (err) => {
+      if (err) throw err;
+      console.log('gh-page, OK');
+    });
+
+    const organizationHtml = (`
+      <!DOCTYPE HTML>
+      <html lang="zh-tw">
+      <head>
+        <meta charset="utf-8">
+        <title>Developer Conferences in Taiwan</title>
+        <meta name="description" content="統整研討會資訊" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content="Developer Conferences in Taiwan" />
+        <meta property="og:description" content="統整研討會資訊" />
+        <meta property="og:url" content="https://dcit.ivanwei.co" />
+        <meta property="og:site_name" content="Developer Conferences in Taiwan" />
+        <meta property="og:image" content="https://blog.ivanwei.co/images/2018/05/16/DCIT.png" />
+        <meta property="og:locale" content="zh_TW" />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="Developer Conferences in Taiwan" />
+        <meta name="twitter:description" content="統整研討會資訊" />
+        <meta name="twitter:image" content="https://blog.ivanwei.co/images/2018/05/16/DCIT.png" />
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/picnic">
+      </head>
+      <body>
+        <a class="button" href="/index.html">查詢今年活動</a>
+        <a class="button" href="https://github.com/IvanWei/developer-conferences-in-taiwan/blob/master/data/list-of-organizations.json">新增活動籌備單位</a>
+        ${converter.makeHtml(json2md(organizationData))}
+      </body>
+      </html>
+    `).replace(/(\r\n|\n|\r|\ \ )/gm, '');
+
+    fs.writeFile('docs/organization.html', organizationHtml, (err) => {
       if (err) throw err;
       console.log('gh-page, OK');
     });
